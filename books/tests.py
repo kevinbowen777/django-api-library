bf06b270552b592d4ce2bc7af2f9b7781f4b4c7d
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-26 15:36:07 -0700

    Write basic Book model tests

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase  # noqa:F401
+from django.test import TestCase
 
-# Create your tests here.
+from .models import Book
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Book.objects.create(
+            title="Django for APIs",
+            subtitle="Build web APIs with Python & Django",
+            author="William S. Vincent",
+            isbn="1093633948",
+        )
+
+    def test_title_content(self):
+        book = Book.objects.get(id=1)
+        expected_object_name = f"{book.title}"
+        self.assertEqual(expected_object_name, "Django for APIs")
+
+    def test_subtitle_content(self):
+        book = Book.objects.get(id=1)
+        expected_object_name = f"{book.subtitle}"
+        self.assertEqual(
+            expected_object_name, "Build web APIs with Python & Django"
+        )
+
+    def test_author_content(self):
+        book = Book.objects.get(id=1)
+        expected_object_name = f"{book.author}"
+        self.assertEqual(expected_object_name, "William S. Vincent")
+
+    def test_isbn_content(self):
+        book = Book.objects.get(id=1)
+        expected_object_name = f"{book.isbn}"
+        self.assertEqual(expected_object_name, "1093633948")
