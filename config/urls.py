781a208d67eb6c4056077cedcf47af73611ee68c
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-27 11:43:40 -0700

    Add api-auth URL. Resolves #13

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
     path("", include("books.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
