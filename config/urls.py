3d8b843ce5486faedf456e369173451375d3ac37
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-27 17:38:47 -0700

    Add registration URL

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("", include("books.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
+    path(
+        "api/v1/dj-rest-auth/registration/",
+        include("dj_rest_auth.registration.urls"),
+    ),
 ]
