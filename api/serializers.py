0f428ff6ffca4d1c135d064d8231038925c9f2be
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-28 17:19:24 -0700

    Create UserSerializer class for new endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from books.models import Book
@@ -7,3 +8,12 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "subtitle", "author", "isbn")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "username",
+        )
