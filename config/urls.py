05576b22939ebff0b4b8f75c9797349fd05f52b8
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-03 11:11:33 -0700

    Add redoc & schema routes. Fixes #45

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.conf import settings  # noqa:F401
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
@@ -18,6 +22,16 @@ urlpatterns = [
         "api/schema/",
         SpectacularAPIView.as_view(),
         name="schema",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
 ]
 
