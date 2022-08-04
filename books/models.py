206ba0c20507463922cc42b45c8b2bf31ede1d83
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-03 18:02:44 -0700

    Allow for blank subtitles

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    subtitle = models.CharField(max_length=250)
+    subtitle = models.CharField(blank=True, max_length=250)
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
 
