5a409a0a2ad99bc101c4cc89643a4b0f4d63f365
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-08 10:07:33 -0700

    Create CustomUser. Resolves #59

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
-from django.db import models  # noqa:F401
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField("Name of User", blank=True, max_length=255)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    bio = models.TextField("Bio", blank=True)
+
+    def __str__(self):
+        return self.username
--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,8 @@ STATICFILE_FINDERS = [
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "accounts.CustomUser"
+
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
